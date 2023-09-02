28ab7ed284f29e375df0dd6117fd46f32408f8e4
sor4chi <sorakawa0218@gmail.com>
2023-09-02 09:12:54 +0900

    feat: implement gen-token form

--- app/routes/_index/register.tsx
+++ app/routes/_index/register.tsx
@@ -3,19 +3,32 @@ import { Button } from "@saitamau-maximum/ui";
 import { TextInput } from "~/components/ui/text-input";
 import styles from "./register.module.css";
 
-export function Register() {
+interface RegisterProps {
+  error?: string;
+  token?: string;
+}
+
+export function Register({ error, token }: RegisterProps) {
   return (
     <section>
       <h2>参加登録</h2>
-      <Form method="post" action="/login" className={styles.form}>
+      {token && (
+        <div className={styles.token}>
+          <p>トークンを発行しました</p>
+          <p>トークン: {token}</p>
+          <p>このトークンは大切に保管してください</p>
+        </div>
+      )}
+      <Form method="post" className={styles.form}>
         <label className={styles.formItem}>
           チーム名
           <TextInput type="text" name="team_name" />
         </label>
         <label className={styles.formItem}>
           認証コード
-          <TextInput type="password" name="password" />
+          <TextInput type="password" name="verification_code" />
         </label>
+        {error && <p className={styles.error}>{error}</p>}
         <Button type="submit">トークン発行</Button>
       </Form>
     </section>
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,20 +1,108 @@
-import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import {
+  json,
+  type LoaderArgs,
+  type V2_MetaFunction,
+} from "@remix-run/cloudflare";
 import { MaxWidthCenterLayout } from "~/components/layout/max-width-center";
 import { Register } from "./register";
 import { SITE_TITLE } from "~/constants/config";
 import { Hero } from "./hero";
 import { Description } from "./description";
+import { client } from "~/db/client.server";
+import { competitions, teams } from "~/db/schema";
+import { eq } from "drizzle-orm";
+import { generateToken } from "~/utils/token";
+import { useActionData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: SITE_TITLE }];
 };
 
+export const action = async ({ context, request }: LoaderArgs) => {
+  const body = await request.formData();
+  const teamName = body.get("team_name");
+  const verificationCode = body.get("verification_code");
+  if (
+    !teamName ||
+    !verificationCode ||
+    typeof teamName !== "string" ||
+    typeof verificationCode !== "string"
+  ) {
+    return json(
+      {
+        type: "error",
+        error: "チーム名と認証コードを入力してください",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const [selectedCompetition, ..._1] = await client(context.env.DB)
+    .select()
+    .from(competitions)
+    .where(eq(competitions.verificationCode, verificationCode));
+
+  if (!selectedCompetition) {
+    return json(
+      {
+        type: "error",
+        error: "認証コードが間違っています",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const [newTeam, ..._2] = await client(context.env.DB)
+    .insert(teams)
+    .values({
+      name: teamName,
+      competitionId: selectedCompetition.id,
+    })
+    .returning({
+      insertedId: teams.id,
+    });
+
+  if (!newTeam) {
+    return json(
+      {
+        type: "error",
+        error: "チームの登録に失敗しました",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const token = await generateToken({
+    secret: context.env.SECRET,
+    teamId: newTeam.insertedId,
+    competitionId: selectedCompetition.id,
+  });
+
+  return json(
+    {
+      type: "success",
+      token,
+    },
+    {
+      status: 200,
+    }
+  );
+};
+
 export default function HomePage() {
+  const data = useActionData<typeof action>();
+
   return (
     <MaxWidthCenterLayout>
       <Hero />
       <Description />
-      <Register />
+      <Register {...data} />
     </MaxWidthCenterLayout>
   );
 }
