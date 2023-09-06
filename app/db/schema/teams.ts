5ed1194520fea0dd7de4b0b48661e687cc00761b
sor4chi <sorakawa0218@gmail.com>
2023-09-06 13:03:01 +0900

    fix: teamを同大会かつ同名で登録できないようにする

--- app/db/schema/teams.ts
+++ app/db/schema/teams.ts
@@ -1,7 +1,13 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text, unique } from "drizzle-orm/sqlite-core";
 
-export const teams = sqliteTable("teams", {
-  id: integer("id").primaryKey().notNull(),
-  name: text("name").notNull(),
-  competitionId: integer("competitionId").notNull(),
-});
+export const teams = sqliteTable(
+  "teams",
+  {
+    id: integer("id").primaryKey().notNull(),
+    name: text("name").notNull(),
+    competitionId: integer("competitionId").notNull(),
+  },
+  (teams) => ({
+    teamUnique: unique().on(teams.name, teams.competitionId),
+  })
+);
---
+++ migrations/0004_lonely_the_renegades.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX `teams_name_competitionId_unique` ON `teams` (`name`,`competitionId`);
