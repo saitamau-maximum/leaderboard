66fef486ddd6411e0073b93c6334795656208c5e
sor4chi <sorakawa0218@gmail.com>
2023-09-06 10:50:32 +0900

    fix: topページの文言を修正

--- app/routes/_index/hero.tsx
+++ app/routes/_index/hero.tsx
@@ -6,11 +6,12 @@ export const Hero = () => {
   return (
     <section className={styles.hero}>
       <h1>{SITE_TITLE}</h1>
-      <p>
+      <p className={styles.description}>
         {SITE_TITLE} は、埼玉大学プログラミングサークル
         <Anchor to="https://x.com/maximum03400346/">Maximum</Anchor>
-        が、サークル内で開催しているISUCON*の練習で使うためのサイトです。
+        が、サークル内で開催している大会のスコアボードです。
         <br />
+        ISUCON*や競技プログラミングなどの大会のスコアボードを運用します。
       </p>
       <small>*「ISUCON」は、LINE株式会社の商標または登録商標です。</small>
     </section>
