26a3451d119f6a576b3d50fc77f888efd59cf9ea
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-01-31 01:15:39 +0900

    chore: configure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,50 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "eslint:recommended",
+    "prettier",
+  ],
+  plugins: ["unused-imports"],
   rules: {
-    "@typescript-eslint/no-unused-vars": {
-      argsIgnorePattern: "^_",
-    },
+    "unused-imports/no-unused-imports": "warn",
+    "no-undef": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+        pathGroups: [],
+      },
+    ],
+    "import/newline-after-import": "warn",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+      },
+    ],
   },
 };
